e470e1a78b83d13f0f144fbd3e8fc93ebf19aca0
Pewrie Bontal <0x@bontal.net>
2024-06-02 19:56:07 +0800

    first commit

---
+++ sql/CreateDB.sql
@@ -0,0 +1,14 @@
+-- Create Database
+
+--  We are just programmers, we are not database administrators.
+--  So let us document all anoying comments. so that we can understand what we are doing.
+
+--  Create Database named LeisureAustralasiaDB
+CREATE DATABASE LeisureAustralasiaDB;
+GO
+--  GO is a batch separator. It is used to group SQL commands into batches which are sent to the server together.
+
+
+-- USE statement is used to select a database whose name is specified as the database_name.
+USE LeisureAustralasiaDB;
+GO
